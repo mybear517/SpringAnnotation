e05b28681aa255a3b35590e3fccb039f688b6d2f
mybear517 <mybear517@gmail.com>
2021-02-25 19:43:22 -0500

    fix bean

--- src/main/java/com/spring/config/AppConfig.java
+++ src/main/java/com/spring/config/AppConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.spring.bean.User;
+import com.spring.bean.Address;
 
 @Configuration
 public class AppConfig {
 	@Bean
 	public User user() {
 		return new User();
+	}
+	
+	@Bean 
+	public Address addr() {
+		  return new Address();
 	}
 
 }
